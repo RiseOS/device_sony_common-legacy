6ad83fdb80bf663b6173921d1c35b45e7c41fec9
Pavel Dubrova <pashadubrova@gmail.com>
2018-11-02 06:02:34 +0200

    hardware: qcom: Include BT HAL
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- hardware/qcom/Android.mk
+++ hardware/qcom/Android.mk
@@ -9,6 +9,7 @@ MSM_VIDC_TARGET_LIST := msm8952 msm8996 msm8998 sdm660 sdm845
 MASTER_SIDE_CP_TARGET_LIST := msm8996 msm8998 sdm660 sdm845
 
 audio-hal := hardware/qcom/audio
+bt-hal := hardware/qcom/bt/msm8998
 gps-hal := hardware/qcom/gps/sdm845
 display-hal := hardware/qcom/display/msm8994
 QCOM_MEDIA_ROOT := hardware/qcom/media/sdm845
@@ -24,5 +25,6 @@ include device/sony/common-legacy/hardware/qcom/utils.mk
 
 include $(display-hal)/Android.mk
 include $(call all-makefiles-under,$(audio-hal))
+include $(call all-makefiles-under,$(bt-hal))
 include $(call all-makefiles-under,$(gps-hal))
 include $(call all-makefiles-under,$(media-hal))
