5ba76b470ea826ba73563a9afd3ec554ff523237
Pavel Dubrova <pashadubrova@gmail.com>
2018-11-02 03:58:34 +0200

    hardware: Remove qca_cld3 stuff
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- hardware/qca_cld3/Android.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(WIFI_DRIVER_BUILT),qca_cld3)
-$(shell mkdir -p $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld)
-$(shell pushd $(PRODUCT_OUT)/$(TARGET_COPY_OUT_VENDOR) > /dev/null && ln -sf /data/vendor/wifi/wlan_mac.bin firmware/wlan/qca_cld/wlan_mac.bin && popd > /dev/null)
-endif
